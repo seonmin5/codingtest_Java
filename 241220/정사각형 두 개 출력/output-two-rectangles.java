1e62197e5857d636a06d54b6c5f54a3fa5659dc0
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-12-20 13:35:12 +0900

    Title: 정사각형 두 개 출력, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241220/정사각형 두 개 출력/output-two-rectangles.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    
+        br.close();
+    }
+}
