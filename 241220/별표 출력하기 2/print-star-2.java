eb63bf6d6ee181d53dcde6a6049fddeb9929b8a5
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-12-20 13:40:03 +0900

    Title: 별표 출력하기 2, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241220/별표 출력하기 2/print-star-2.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        br.close();
+    }
+}
