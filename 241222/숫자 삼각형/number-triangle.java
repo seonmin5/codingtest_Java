ed80c9705d8d47e401d8a6805c9162d05463e222
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-12-22 18:46:43 +0900

    Title: 숫자 삼각형, Time: 84ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241222/숫자 삼각형/number-triangle.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.printf("%d ", j);
+            }
+            System.out.println();
+        }
+        br.close();
+    }
+}
