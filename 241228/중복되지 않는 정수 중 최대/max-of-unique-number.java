d4ce5c04087e328c740faa34629d1535a6801fa1
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-12-28 23:40:51 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 100ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241228/중복되지 않는 정수 중 최대/max-of-unique-number.java
+++ 241228/중복되지 않는 정수 중 최대/max-of-unique-number.java
@@ -2,33 +2,34 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.HashMap;
 
 public class Main {
-    private static int maxResult(int arr[]) {
-        int max = arr[0];
-        int count = 0;
-        for (int i = 1; i < arr.length; i++) {
-            if (max == arr[i]) {
-                max = -1;
-                count++;
-            } else if (arr[i] > max && count == 0) {
-                max = arr[i];
-            }
-        }
-        return max;
-    }
-
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine().trim());
         int arr[] = new int[n];
-
         StringTokenizer st = new StringTokenizer(br.readLine().trim());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
         System.out.print(maxResult(arr));
         br.close();
+    }
+
+    private static int maxResult(int arr[]) {
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        for (int num : arr) {
+            countMap.put(num, countMap.getOrDefault(num, 0)+1);
+        }
+        int max = arr[0];
+        boolean found = false;
+        for (int num : countMap.keySet()) {
+            if (countMap.get(num) == 1) {
+                max = Math.max(max, num);
+                found = true;
+            }
+        }
+        return found ? max : -1;
     }
 }
