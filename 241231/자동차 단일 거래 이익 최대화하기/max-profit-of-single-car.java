4915157350bb99f563145f0cd520c747a5c51e9b
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-12-31 22:18:50 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 104ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241231/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+        int arr[] = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.print(profit(arr));
+        br.close();
+    }
+
+    private static int profit(int arr[]) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int price : arr) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+        return maxProfit;
+    }
+}
