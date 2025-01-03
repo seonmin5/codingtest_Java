10821c4e23a584debf0dd1ab731b2d59d4476d7d
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2025-01-03 14:35:14 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.09 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        
+        for (char c: my_string.toCharArray()) {
+            if (c < 'a') {
+                answer[c-'A']++;
+            } else {
+                answer[26+c-'a']++;
+            }
+        }
+        
+        return answer;
+    }
+}
