de93cf476fa9493e9d4210a4954d8a466ffbb784
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2025-01-23 13:33:35 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.16 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String s) {
+        String arr[] = s.split(" ");
+        int result = 0;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("Z")) {
+                result -= Integer.parseInt(arr[i-1]);
+            } else {
+                result += Integer.parseInt(arr[i]);
+            }
+        }
+        
+        return result;
+    }
+}
