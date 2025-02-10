0dacf899f3d09b75018c6e9b8be991cbbc35c6ae
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2025-02-10 14:05:02 +0900

    [level 0] Title: 안전지대, Time: 4.21 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board) {
+        // 1의 위치: 2차 배열로
+        int n = board.length;
+        int m = board[0].length;
+        List<int[]> oneBoard = new ArrayList<>();
+        for (int i = 0 ; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 1) {
+                    oneBoard.add(new int[]{i, j});
+                }
+            }
+        }
+        
+        // 8방 탐색: 위험지역 표시
+        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+        for (int[] pos : oneBoard) {
+            int x = pos[0];
+            int y = pos[1];
+            
+            for (int d = 0; d < 8; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                
+                if (nx >=0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0) {
+                    board[nx][ny] = 1;
+                }
+            }
+        }
+        
+        // 안전지역 개수 return
+        return (int) Arrays.stream(board)
+            .flatMapToInt(Arrays::stream)
+            .filter(b -> b == 0)
+            .count();
+    }
+}
