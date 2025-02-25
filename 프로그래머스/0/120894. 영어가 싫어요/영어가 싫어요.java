46a2162d04ee7d92f228ce68badfd7dd8ae83d63
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2025-02-25 12:02:16 +0900

    [level 0] Title: 영어가 싫어요, Time: 1.24 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,26 +1,9 @@
-import java.util.*;
-
 class Solution {
     public long solution(String numbers) {
-        HashMap<String, Integer> map = new HashMap<>();
         String arr[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        for (int i = 0; i < 10; i++) {
-            map.put(arr[i], i);
+        for (int i = 0; i < arr.length; i++) {
+            numbers = numbers.replaceAll(arr[i], String.valueOf(i));
         }
-        
-        int index = 0;
-        StringBuilder result = new StringBuilder();
-        while (index < numbers.length()) {
-            for (String key : map.keySet()) {
-                if (numbers.startsWith(key, index)) {
-                    result.append(map.get(key));
-                    index += key.length();
-                    break;
-                }
-            }
-        }
-        
-        long answer = Long.parseLong(result.toString());
-        return answer;
+        return Long.parseLong(numbers);
     }
 }
