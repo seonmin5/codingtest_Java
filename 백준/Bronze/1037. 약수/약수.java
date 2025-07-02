fb4604eabbfb8874827b59375ff871be2d2b1c93
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2025-07-02 16:57:26 +0900

    [Bronze I] Title: 약수, Time: 108 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -1,27 +1,20 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[N];
+        int max = -1;
+        int min = Integer.MAX_VALUE;
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        while (N-- > 0) {
+            int n = Integer.parseInt(st.nextToken());
+            max = Math.max(max, n);
+            min = Math.min(min, n);
         }
 
-        int MAX = -1;
-        int MIN = -1;
-        if (N == 1) {
-            MAX = arr[0];
-            System.out.println(MAX*MAX);
-        } else {
-            MAX = Arrays.stream(arr).max().getAsInt();
-            MIN = Arrays.stream(arr).min().getAsInt();
-            System.out.println(MAX*MIN);
-        }
-
+        System.out.println(max * min);
     }
 }
